5d240408e4ac2f0e308c0f20f1d09e96eb4a056a
Trần Chí Quốc <quoct0447@gmail.com>
2026-03-06 15:26:01 +0700

    updat section Home

--- web_ban_giay_ecommerce/src/App.jsx
+++ web_ban_giay_ecommerce/src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
 import "./App.css";
-import Home from "./components/sections/Home";
 
 function App() {
-  return <div>
-    <Home/>
-  </div>;
+  return <div>App</div>;
 }
 
 export default App;
